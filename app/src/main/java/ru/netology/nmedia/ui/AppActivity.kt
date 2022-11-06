5781c9d163cb59dd3f10f8b29574934d99437441
pvshmyglev <pshm2@mail.ru>
2022-11-06 19:03:47 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -0,0 +1,22 @@
+package ru.netology.nmedia.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.R
+
+class AppActivity : AppCompatActivity(R.layout.activity_app) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        findViewById<StatsView>(R.id.stats).data = listOf(
+            10F,
+            10F,
+            10F,
+            10F,
+            10F,
+            10F,
+            10F,
+            10F,
+        )
+    }
+}
